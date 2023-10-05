385302c25cbe202ac0c85137ea77420eec2a6128
onahanthony <anthonyonah18@gmail.com>
2023-10-05 18:58:10 +0000

    done

--- sg.tf
+++ sg.tf
@@ -5,15 +5,15 @@ resource "aws_security_group" "http-sg" {
 
   ingress {
     description = "from my ip range"
-    from_port   = "80"
-    to_port     = "80"
+    from_port   =  80
+    to_port     =  80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     description = "from my ip range"
-    from_port   = "8080"
-    to_port     = "8080"
+    from_port   =  8080
+    to_port     =  8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
