a44fdcc6a13eac1669910feaa94c42a21c715603
onahanthony <anthonyonah18@gmail.com>
2023-10-05 18:17:58 +0000

    done

---
+++ instance.tf
@@ -0,0 +1,25 @@
+resource "aws_instance" "app-server1" {
+  instance_type               = "t2.micro"
+  ami                         = "ami-053b0d53c279acc90"
+  key_name                    = "ssh-key"
+  vpc_security_group_ids      = [aws_security_group.http-sg.id]
+  subnet_id                   = aws_subnet.public-sb1.id
+  associate_public_ip_address = true
+
+  user_data = <<EOF
+#!/bin/bash
+sudo apt update
+sudo apt install apache2 -y
+EOF
+
+  tags = {
+    Name = "app-server-1"
+  }
+ 
+}
+
+output "ec2_global_ips" {
+  value = aws_instance.app-server1.public_ip
+}
+
+
