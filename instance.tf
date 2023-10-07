90f1452821c2ddf862b5fe7e49232486b5a7cd1d
Onah Anthony <anthonyonah18@gmail.com>
2023-10-07 03:25:57 +0100

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "app-server2" {
     Name = "app-server-2"
   }
   provisioner "local-exec" {
-   command = "echo ${self.public_ip} > /home/runner/inventory"
+   command = "echo ${self.public_ip} > /home/runner/work/github-action/github-action/inventory"
   }
  
 }
